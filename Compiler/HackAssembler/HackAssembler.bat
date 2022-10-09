eca2ead02341322156986f72a604633eece4407e
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2022-10-09 21:28:42 +0100

    read file

---
+++ Compiler/HackAssembler/HackAssembler.bat
@@ -0,0 +1,2 @@
+"C:\Users\Thomas.Hallam\git-repos\personal\Compiler\Compiler\HackAssembler\bin\Release\net6.0\HackAssembler.exe" %1
+pause
--- Compiler/HackAssembler/Program.cs
+++ Compiler/HackAssembler/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var path = args[0];
 
-Console.WriteLine("Hello, World!");
+if (Path.GetExtension(path) != ".asm")
+{
+    await Console.Error.WriteLineAsync($"File path '{path}' does not have the .asm extension.");
+    return ;
+}
+else if (!File.Exists(path))
+{
+    await Console.Error.WriteLineAsync($"File '{path}' does not exist.");
+    return ;
+}
+
+var fileLines = await File.ReadAllLinesAsync(path);
+
+foreach (var fileLine in fileLines)
+{
+    Console.WriteLine(fileLine);
+}
