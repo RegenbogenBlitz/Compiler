ff8c54ed10f9168e33ab4f3300f7643004fbb41b
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2022-10-11 21:17:52 +0100

    add basic vmtranslator that just returns what it receives

---
+++ Compiler/VmHackAsmTranslator/Program.cs
@@ -0,0 +1,28 @@
+﻿var path = args[0];
+
+if (Path.GetExtension(path) != ".vm")
+{
+    await Console.Error.WriteLineAsync($"File path '{path}' does not have the .vm extension.");
+    return ;
+}
+
+var fileInfo = new FileInfo(path);
+if (!fileInfo.Exists)
+{
+    await Console.Error.WriteLineAsync($"File '{path}' does not exist.");
+    return ;
+}
+
+var vmFilesDirectory = fileInfo.Directory;
+if (vmFilesDirectory == null)
+{
+    throw new IOException($"File '{path}' existed but directory now does not.");
+}
+
+var fileLines = await File.ReadAllLinesAsync(path);
+
+
+var outputFileInfo = new FileInfo(Path.Join(vmFilesDirectory.FullName, Path.GetFileNameWithoutExtension(path) + ".asm"));
+File.WriteAllLines(outputFileInfo.FullName, fileLines);
+
+Console.Write(outputFileInfo.FullName);
---
+++ Compiler/VmHackAsmTranslator/VmHackAsmTranslator.bat
@@ -0,0 +1,2 @@
+"C:\Users\Thomas.Hallam\git-repos\personal\Compiler\Compiler\VmHackAsmTranslator\bin\Release\net6.0\VmHackAsmTranslator.exe" %1
+pause
