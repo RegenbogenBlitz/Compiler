a64d69f22a63fa325ffb7706eb097bb080b525b3
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2022-10-11 23:13:15 +0100

    implement simple binary and unary operators

--- Compiler/VmHackAsmTranslator/VmTranslator.cs
+++ Compiler/VmHackAsmTranslator/VmTranslator.cs
@@ -36,7 +36,27 @@ public static class VmTranslator
                     break;
 
                 case "add":
-                    output += WriteAdd();
+                    output += WriteBinaryOperator("+", "Add", "+");
+                    break;
+                
+                case "sub":
+                    output += WriteBinaryOperator("-", "Subtract", "-");
+                    break;
+                
+                case "neg":
+                    output += WriteUnaryOperator("-", "Negative", "-");
+                    break;
+                
+                case "and":
+                    output += WriteBinaryOperator("&", "And", "and");
+                    break;
+                
+                case "or":
+                    output += WriteBinaryOperator("|", "Or", "or");
+                    break;
+                
+                case "not":
+                    output += WriteUnaryOperator("!", "Not", "not ");
                     break;
                 
                 default:
@@ -71,7 +91,7 @@ public static class VmTranslator
             case "constant":
                 return
                     OpenSectionComment($"Push Constant '{index}'", 0) +
-                    AInstruction(index, 1) +
+                    AInstruction(index) +
                     PadLine("D=A") + Comment($"{index} => D", 1) +
                     PushD(1) +
                     CloseSectionComment(0);
@@ -80,73 +100,80 @@ public static class VmTranslator
                 return TrimLine(line);
         }
     }
-    
-    private static string WriteAdd() =>
-        OpenSectionComment("Add", 0)  +
+
+    private static string WriteUnaryOperator(string operatorSymbol, string operatorName, string commentOperator) =>
+        OpenSectionComment(operatorName, 0) +
+        DropStack(1) +
+        OperatorMemoryToMemory(operatorSymbol, commentOperator, 1) +
+        LiftStack(1) +
+        CloseSectionComment(0);
+
+    private static string WriteBinaryOperator(string operatorSymbol, string operatorName, string commentOperator) =>
+        OpenSectionComment(operatorName, 0) +
         PopToD(1) +
         DToMemory("R13", 1) +
         PopToD(1) +
-        DPlusMemoryToD("R13", 1) +
+        DOperatorMemoryToD("R13", operatorSymbol, commentOperator, 1) +
         PushD(1) +
         CloseSectionComment(0);
 
     private static string SetMemoryToValue(string memoryAddress, string value, int indentation) =>
         OpenSectionComment($"Set {memoryAddress} to '{value}'", indentation) +
-        AInstruction(value, indentation + 1) +
+        AInstruction(value) +
         PadLine("D=A") + Comment($"{value} => D", indentation + 1) +
         DToMemory(memoryAddress, indentation + 1) +
         CloseSectionComment(indentation);
 
     private static string PushD(int indentation) =>
-        OpenSectionComment("Push D", indentation) +
-        DToTopStack(indentation + 1) +
-        LiftStack(indentation + 1) +
-        CloseSectionComment(indentation);
-    
+        DToTopStack(indentation) +
+        LiftStack(indentation);
+
     private static string PopToD(int indentation) =>
-        OpenSectionComment("Pop to D", indentation) +
-        DropStack(indentation +1 ) +
-        TopStackToD(indentation +1 ) +
-        CloseSectionComment(indentation);
-    
+        DropStack(indentation) +
+        TopStackToD(indentation);
+
     private static string DToMemory(string memoryAddress, int indentation) =>
-        AInstruction(memoryAddress, indentation) +
+        AInstruction(memoryAddress) +
         PadLine("M=D") + Comment($"D => {memoryAddress}", indentation);
-    
-    private static string DPlusMemoryToD(string memoryAddress, int indentation) =>
-        AInstruction(memoryAddress, indentation) +
-        PadLine("D=D+M") + Comment($"D + {memoryAddress} => D", indentation);
-    
+
+    private static string DOperatorMemoryToD(string memoryAddress, string operatorSymbol, string commentOperator, int indentation) =>
+        AInstruction(memoryAddress) +
+        PadLine($"D=D{operatorSymbol}M") + Comment($"D {commentOperator} {memoryAddress} => D", indentation);
+
+    private static string OperatorMemoryToMemory(string operatorSymbol, string commentOperator, int indentation) =>
+        PadLine("A=M") + Environment.NewLine +
+        PadLine($"M={operatorSymbol}M") + Comment($"{commentOperator}M => M", indentation);
+
     private static string DToTopStack(int indentation) =>
-        AInstruction("SP", indentation) +
-        PadLine("A=M") + Comment("", indentation) +
+        AInstruction("SP") +
+        PadLine("A=M") + Environment.NewLine +
         PadLine("M=D") + Comment("D => TopStack", indentation);
-    
+
     private static string TopStackToD(int indentation) =>
-        PadLine("A=M") + Comment("", indentation) +
+        PadLine("A=M") + Environment.NewLine +
         PadLine("D=M") + Comment("TopStack => D", indentation);
     
     private static string LiftStack(int indentation) =>
-        AInstruction("SP", indentation) +
+        AInstruction("SP") +
         PadLine("M=M+1") + Comment("Lift Stack", indentation);
     
     private static string DropStack(int indentation) =>
-        AInstruction("SP", indentation) +
+        AInstruction("SP") +
         PadLine("M=M-1") + Comment("Drop Stack", indentation);
-    
-    private static string AInstruction(string value, int indentation)
-        => PadLine($"@{value}") + Comment("", indentation);
+
+    private static string AInstruction(string value)
+        => PadLine($"@{value}") + Environment.NewLine;
     
     private static string PadLine(string value)
-        => value.PadRight(6, ' ');
+        => value.PadRight(25, ' ');
     
     private static string OpenSectionComment(string comment, int indentation)
-        => PadLine("") + "// " + "".PadRight(indentation * 3, ' ') + "[" + comment +  "] {" + Environment.NewLine;
+        => PadLine("") + " // " + "".PadRight(indentation * 3, ' ') + "[" + comment +  "] {" + Environment.NewLine;
 
     private static string Comment(string comment, int indentation)
-        => "// "+ "".PadRight(indentation * 3, ' ') + comment + Environment.NewLine;
+        => " // "+ "".PadRight(indentation * 3, ' ') + comment + Environment.NewLine;
     
     private static string CloseSectionComment(int indentation)
-        => PadLine("") + "// "+ "".PadRight(indentation * 3, ' ') + "}" + Environment.NewLine;
+        => PadLine("") + " // "+ "".PadRight(indentation * 3, ' ') + "}" + Environment.NewLine;
     
 }
